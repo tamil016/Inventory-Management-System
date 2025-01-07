13fa29738f2b8070cdc4c093b4cf6125d86e7d8d
tamil016 <sendmailtotamilarasan@gmail.com>
2025-01-07 12:05:17 +0530

    Testing completed

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,32 +2,33 @@ const Product = require('../models/Product');
 
 const searchProduct = async (req, res) => {
     try {
-        const { name, category, minPrice, maxPrice, minQuantity, maxQuantity } = req.query;
+        const { name, category, sortBy, page=1 } = req.query;
 
         let filter = {};
-
         if (name) filter.name = new RegExp(name, 'i');
-
         if (category) filter.category = category;
 
-        if (minPrice || maxPrice) {
-            filter.price = {};
-            if (minPrice) filter.price.$gte = Number(minPrice);
-            if (maxPrice) filter.price.$lte = Number(maxPrice);
+        let sortCriteria = {};
+        if (sortBy) {
+            const parsedSortBy = JSON.parse(sortBy);
+            const sortByField = parsedSortBy.field || "name";
+            const sortByOrder = parsedSortBy.order === "desc" ? -1 : 1;
+            sortCriteria[sortByField] = sortByOrder;
+        } else {
+            sortCriteria.name = 1;
         }
+        // console.log(sortCriteria);
+        const limit = 6;
+        const skip = (page - 1) * limit;
 
-        if (minQuantity || maxQuantity) {
-            filter.quantity = {};
-            if (minQuantity) filter.quantity.$gte = Number(minQuantity);
-            if (maxQuantity) filter.quantity.$lte = Number(maxQuantity);
-        }
-
-        const products = await Product.find(filter).populate('category');
-
-        res.json(products);
+        const totalProducts = await Product.countDocuments(filter);
+        const totalPages = Math.ceil(totalProducts / limit);
+        
+        const products = await Product.find(filter).skip(skip).limit(limit).populate('category').sort(sortCriteria);
+        res.json({products, totalPages, currentPage: Number(page)});
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-}
+};
 
-module.exports = { searchProduct }
+module.exports = { searchProduct };
