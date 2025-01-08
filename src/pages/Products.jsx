f8ee8238e1dcac5dd88117fb0aadb1dff99b66f5
tamil016 <sendmailtotamilarasan@gmail.com>
2025-01-08 11:08:33 +0530

    Product Validation done

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -93,6 +93,9 @@ const Products = () => {
   };
 
   const handleSaveProduct = async () => {
+    if(!currentProduct.name || !currentProduct.category || !currentProduct.quantity || !currentProduct.price || !currentProduct.description){
+      toast.error('All fields are required')
+    }
     if (!currentProduct.name.trim()) {
       toast.error('Product name is required.');
       return;
